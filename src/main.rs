472554a20d36a74c4aadb8568d515bacbbc575d2
AC Che <caxche@gmail.com>
2022-10-24 21:58:41 +0800

    update history.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod ssq;
 pub fn main() {
     let ssq_pool = ssq::get_ssq_from_file();
-    let ssq = ssq::gen_by_user(21, 33, 6, &ssq_pool);
+    let ssq = ssq::gen_by_user(5, 33, 6, &ssq_pool);
     println!("ssq = {:?}", ssq);
 }
